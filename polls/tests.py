c3d0cd37a0ff19ea9698edd4b242ed7c169acaa8
Çağrı Bıyık <cagribiyik@Cagrs-MacBook-Pro.local>
2019-07-30 14:05:04 +0300

    login test

--- polls/tests.py
+++ polls/tests.py
@@ -6,3 +6,14 @@ class TestCalls(TestCase):
     def test_call_view_loads(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200, msg='View is not reachable')
+    
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        response = self.client.post('/login/', self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_active)
+        
