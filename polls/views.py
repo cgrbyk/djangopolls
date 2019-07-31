411e18457d62b67b6e02221a732c776430acb094
Çağrı Bıyık <cagribiyik@Cagrs-MacBook-Pro.local>
2019-07-31 13:01:33 +0300

    questionlist added

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Question,Choice
 
 # Create your views here.
+
+def questionlist(request):
+    questions = Question.objects.all()
+    return render(request,'questionlist.html',{'questions':questions})
