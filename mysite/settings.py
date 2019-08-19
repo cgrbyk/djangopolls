46c265611097b99019980a8949a2382a48571027
cgrbyk <cgrbyk@gmail.com>
2019-08-19 16:35:08 +0300

    jenkinsfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #        'NAME': os.environ.get('DB_ENV_DB', 'postgres'),
 #        'USER': os.environ.get('DB_ENV_POSTGRES_USER', 'postgres'),
 #        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'postgres'),
-#        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', 'db'),
+#        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', '127.0.0.1'),
 #        'PORT': os.environ.get('DB_PORT_5432_TCP_PORT', ''),
 #    },
 #}
